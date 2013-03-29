c27ad29d89ff3e0f1b867e0a015cbc0ae73b522b
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-03-29 13:24:00 +0000

    Add encoding for multi-field

---
+++ c/src/main/java/edu/nyu/cascade/c/theory/LISBQDArrReachTheory.java
@@ -0,0 +1,28 @@
+package edu.nyu.cascade.c.theory;
+
+import edu.nyu.cascade.c.Theory;
+import edu.nyu.cascade.ir.expr.LISBQwithQFDArrReachEncoding;
+import edu.nyu.cascade.ir.expr.ReachMemoryModel;
+import edu.nyu.cascade.prover.ExpressionManager;
+import edu.nyu.cascade.util.Preferences;
+
+public class LISBQDArrReachTheory implements Theory {
+  private final LISBQwithQFDArrReachEncoding encoding;
+  private final ReachMemoryModel memoryModel;
+  
+  public LISBQDArrReachTheory(ExpressionManager exprManager) {
+    Preferences.set(Preferences.OPTION_ENCODE_FIELD_ARRAY);
+    encoding = new LISBQwithQFDArrReachEncoding(exprManager);
+    memoryModel = LISBQwithQFDArrReachEncoding.createMemoryModel(encoding);
+  }
+
+  @Override
+  public LISBQwithQFDArrReachEncoding getEncoding() {
+    return encoding;
+  }
+
+  @Override
+  public ReachMemoryModel getMemoryModel() {
+    return memoryModel;
+  }
+}
