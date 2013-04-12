023a7d0a224327c57025631cb107037df0b947fa
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-04-12 20:53:25 +0000

    update script file with both z3 and cvc4 enabled

--- c/src/main/scripts/cascade.sh
+++ c/src/main/scripts/cascade.sh
@@ -2,9 +2,14 @@
 
 Z3_LIB_DIR="${z3.libdir}"
 Z3_JAR="${z3.jar}"
+CVC4_LIB_DIR="${cvc4.libdir}"
+CVC4_JNI_LIB_DIR="${cvc4.jni.libdir}"
+CVC4_JAR="${cvc4.jar}"
+
 CASCADE_BIN_DIR="${project.build.directory}"
 CASCADE_JAR="$CASCADE_BIN_DIR/${project.build.finalName}-jar-with-dependencies.jar"
 
-DYLD_LIBRARY_PATH=$Z3_LIB_DIR:$DYLD_LIBRARY_PATH
+DYLD_LIBRARY_PATH=$Z3_LIB_DIR:$CVC4_LIB_DIR:$CVC4_JNI_LIB_DIR:$DYLD_LIBRARY_PATH
+LD_LIBRARY_PATH=$Z3_LIB_DIR:$CVC4_LIB_DIR:$CVC4_JNI_LIB_DIR:$LD_LIBRARY_PATH
 
-java -ea -cp "$Z3_JAR:$CASCADE_JAR" edu.nyu.cascade.c.Main "$@"
+java -ea -cp "$Z3_JAR:$CVC4_JAR:$CASCADE_JAR" edu.nyu.cascade.c.Main "$@"
