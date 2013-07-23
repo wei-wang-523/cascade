b8b65132c0f2df5898b008c16bed4eaaf66eae7e
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-07-23 17:42:17 +0000

    change bnc names

--- c/src/test/java/edu/nyu/cascade/c/MainTest.java
+++ c/src/test/java/edu/nyu/cascade/c/MainTest.java
@@ -36,9 +36,9 @@ public class MainTest {
   private static final File programs_test_location = new File(programs_c_location,
       "test");
   private static final File mini_programs_location = new File(programs_test_location,
-      "minicase_bug");
+      "mini");
   private static final File nec_programs_location = new File(programs_test_location,
-      "testcase_nec");
+      "nec");
   private static final FilenameFilter cFileFilter = new FilenameFilter() {
     public boolean accept(File dir, String name) {
       return name.endsWith(".c");
