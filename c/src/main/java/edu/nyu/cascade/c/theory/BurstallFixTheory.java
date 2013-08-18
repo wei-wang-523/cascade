71056772f0cd1b6e834db9c42228180d7694fa5d
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-08-18 15:13:25 +0000

    BurstallFix theory for Burstall model with various size for different types

--- c/src/main/java/edu/nyu/cascade/c/theory/BurstallFixTheory.java
+++ c/src/main/java/edu/nyu/cascade/c/theory/BurstallFixTheory.java
@@ -1,19 +1,19 @@
 package edu.nyu.cascade.c.theory;
 
 import edu.nyu.cascade.c.Theory;
-import edu.nyu.cascade.ir.expr.BurstallVer0MemoryModel;
+import edu.nyu.cascade.ir.expr.BurstallVer1MemoryModel;
 import edu.nyu.cascade.ir.expr.ExpressionEncoding;
 import edu.nyu.cascade.ir.expr.MemoryModel;
 import edu.nyu.cascade.ir.expr.PointerExpressionEncoding;
 import edu.nyu.cascade.prover.ExpressionManager;
 
-public class BurstallTheory implements Theory {
+public class BurstallFixTheory implements Theory {
   private final ExpressionEncoding encoding;
   private final MemoryModel memoryModel;
   
-  public BurstallTheory(ExpressionManager exprManager) {
+  public BurstallFixTheory(ExpressionManager exprManager) {
     encoding = PointerExpressionEncoding.create(exprManager);
-    memoryModel = BurstallVer0MemoryModel.create(encoding);
+    memoryModel = BurstallVer1MemoryModel.create(encoding);
   }
 
   @Override
