7014f7459857200210c1a77394c6bd3d2156dde6
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-09-28 04:09:56 +0000

    Add burstall view theory: burstall model + view

--- c/src/main/java/edu/nyu/cascade/c/theory/BurstallViewTheory.java
+++ c/src/main/java/edu/nyu/cascade/c/theory/BurstallViewTheory.java
@@ -1,19 +1,19 @@
 package edu.nyu.cascade.c.theory;
 
 import edu.nyu.cascade.c.Theory;
-import edu.nyu.cascade.ir.expr.BurstallMemoryModel;
+import edu.nyu.cascade.ir.expr.BurstallView2MemoryModel;
 import edu.nyu.cascade.ir.expr.ExpressionEncoding;
 import edu.nyu.cascade.ir.expr.MemoryModel;
 import edu.nyu.cascade.ir.expr.PointerExpressionEncoding;
 import edu.nyu.cascade.prover.ExpressionManager;
 
-public class BurstallTheory implements Theory {
+public class BurstallViewTheory implements Theory {
   private final ExpressionEncoding encoding;
   private final MemoryModel memoryModel;
   
-  public BurstallTheory(ExpressionManager exprManager) {
+  public BurstallViewTheory(ExpressionManager exprManager) {
     encoding = PointerExpressionEncoding.create(exprManager);
-    memoryModel = BurstallMemoryModel.create(encoding);
+    memoryModel = BurstallView2MemoryModel.create(encoding);
   }
 
   @Override
