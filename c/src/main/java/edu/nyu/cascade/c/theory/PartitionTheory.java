38fdd14bf4f0f50440cfc41a674e910f88100be4
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-10-14 03:02:02 +0000

    Decouple the memory encoding: 	- memory model: deal with big picture of partition the memory 	- heap encoding: deal with linear and sync encoding 	- order/sound wrapper: deal with memory safe predicates

--- c/src/main/java/edu/nyu/cascade/c/theory/PartitionTheory.java
+++ c/src/main/java/edu/nyu/cascade/c/theory/PartitionTheory.java
@@ -2,23 +2,41 @@ package edu.nyu.cascade.c.theory;
 
 import edu.nyu.cascade.c.Theory;
 import edu.nyu.cascade.ir.expr.ExpressionEncoding;
+import edu.nyu.cascade.ir.expr.IRHeapEncoding;
+import edu.nyu.cascade.ir.expr.IROrderMemLayoutEncoding;
+import edu.nyu.cascade.ir.expr.IRPartitionHeapEncoder;
+import edu.nyu.cascade.ir.expr.IRSoundMemLayoutEncoding;
+import edu.nyu.cascade.ir.expr.LinearHeapEncoding;
 import edu.nyu.cascade.ir.expr.MemoryModel;
-import edu.nyu.cascade.ir.expr.PartitionMemoryModelOrder;
-import edu.nyu.cascade.ir.expr.PartitionMemoryModelSound;
+import edu.nyu.cascade.ir.expr.OrderMemLayoutEncodingFactory;
+import edu.nyu.cascade.ir.expr.PartitionHeapEncoder;
+import edu.nyu.cascade.ir.expr.PartitionMemoryModel;
 import edu.nyu.cascade.ir.expr.PointerExpressionEncoding;
+import edu.nyu.cascade.ir.expr.SoundMemLayoutEncodingFactory;
+import edu.nyu.cascade.ir.expr.SynchronousHeapEncoding;
 import edu.nyu.cascade.prover.ExpressionManager;
 import edu.nyu.cascade.util.Preferences;
 
 public class PartitionTheory implements Theory {
   private final ExpressionEncoding encoding;
+  private final IRPartitionHeapEncoder heapEncoder;
   private final MemoryModel memoryModel;
   
   public PartitionTheory(ExpressionManager exprManager) {
     encoding = PointerExpressionEncoding.create(exprManager);
-    if(Preferences.isSet(Preferences.OPTION_ORDER_ALLOC))
-    	memoryModel = PartitionMemoryModelOrder.create(encoding);
-    else
-    	memoryModel = PartitionMemoryModelSound.create(encoding);	
+    if(Preferences.isSet(Preferences.OPTION_ORDER_ALLOC)) {
+    	IRHeapEncoding heapEncoding = LinearHeapEncoding.create(encoding);
+    	IROrderMemLayoutEncoding memLayout = OrderMemLayoutEncodingFactory
+    			.create(heapEncoding);
+    	heapEncoder = PartitionHeapEncoder.createOrderEncoding(heapEncoding, memLayout);
+    } else {
+    	IRHeapEncoding heapEncoding = LinearHeapEncoding.create(encoding);
+    	IRHeapEncoding heapEncoding_sync = SynchronousHeapEncoding.create(encoding);
+    	IRSoundMemLayoutEncoding memLayout = SoundMemLayoutEncodingFactory
+    			.create(heapEncoding);
+    	heapEncoder = PartitionHeapEncoder.createSoundEncoding(heapEncoding, memLayout);
+    }
+  	memoryModel = PartitionMemoryModel.create(encoding, heapEncoder);	
   }
 
   @Override
