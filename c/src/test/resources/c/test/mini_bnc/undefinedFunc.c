ff4233e04bd033630e343eb73557557c4a509a40
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-11-22 19:55:19 +0000

    Add support of call graph

--- c/src/test/resources/c/test/mini_bnc/undefinedFunc.c
+++ c/src/test/resources/c/test/mini_bnc/undefinedFunc.c
@@ -1,12 +1,12 @@
 int returnOne();
 int addOne(int x, int y);
-
-
+int foo(int x);
+int bar(int x);
 int main() {
   int x = 1;
-  int y;
+  int y = foo(x);
   
-  y = addOne(1, returnOne());
+  y = addOne(bar(x), returnOne());
   ASSERT(y == x+1);
   return y;
 }
