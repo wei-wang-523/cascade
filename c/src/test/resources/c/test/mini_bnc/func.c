759cb96a28d8e937eef118fbdedde52e343c3d57
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-11-22 14:15:26 +0000

    Modify benchmark to test unimplemented function calling

--- c/src/test/resources/c/test/mini_bnc/func.c
+++ c/src/test/resources/c/test/mini_bnc/func.c
@@ -1,3 +1,16 @@
+int returnOne();
+int addOne(int x, int y);
+int foo(int x);
+int bar(int x);
+int main() {
+  int x = 1;
+  int y = foo(x);
+  bar(x);
+  y = addOne(x, returnOne());
+  ASSERT(y == x+1);
+  return y;
+}
+
 int returnOne() {
 	return 1;
 }
@@ -5,12 +18,4 @@ int returnOne() {
 int addOne(int x, int y) {
 	x=x+y;
 	return x;
-}
-
-int main() {
-  int x = 1;
-  int y; 
-  y = addOne(x, returnOne());
-  ASSERT(y == x+1);
-  return y;
 }
