b4c30b4a0d14df76762198d37e9b9c30aca9ca40
wwang1109 <wwang1109@0979de3b-1556-0410-81eb-980c3fe2c0d2>
2013-12-19 22:44:45 +0000

    add benchmarks

--- c/src/test/resources/c/test/cast_bnc/ptrCast.c
+++ c/src/test/resources/c/test/cast_bnc/ptrCast.c
@@ -1,8 +1,9 @@
-int main(){
-	int *p;
+int main(int *p, char *q){
+	int *p = (int *)malloc(sizeof(int));
 	char *q = (char*)malloc(sizeof(char));
 	*q = 'a';
-	p = (int *)q;
-	ASSERT( *p == 'a');
+	p = (int*)q;
+	*p = 'b';
+	ASSERT( *q == 'a');
 	return (int) *p;
 }
