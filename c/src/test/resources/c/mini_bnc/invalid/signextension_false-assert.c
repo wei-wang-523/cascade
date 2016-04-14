9fc4b3e11dc0e43d26b1398ccad4de47409c8f5e
Wei-Wang-523 <wwang1109@cs.nyu.edu>
2016-04-13 18:17:56 -0700

    benchmark change from reach error to assert failure

--- c/src/test/resources/c/mini_bnc/invalid/signextension_false-assert.c
+++ c/src/test/resources/c/mini_bnc/invalid/signextension_false-assert.c
@@ -5,12 +5,7 @@ int main() {
   unsigned short int allbits = -1;
   short int signedallbits = allbits;
   unsigned int signedtounsigned = signedallbits;
-  
-  if (signedtounsigned == 4294967295) {
-    goto ERROR;
-  }
+  ASSERT(signedtounsigned != 4294967295);
   
   return (0);
-ERROR: __VERIFIER_error();
-  return (-1);
 }
